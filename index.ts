02d3dced00f5553fe2c2129f6aa306aed735a24e
Emma Kuo <emma.c.kuo@gmail.com>
2016-09-07 18:55:06 -0700

    change err type to any

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-export function promisify<T>(f: (cb: (err: NodeJS.ErrnoException, res: T) => void) => void): () => Promise<T>;
-export function promisify<A,T>(f: (arg: A, cb: (err: NodeJS.ErrnoException, res: T) => void) => void): (arg: A) => Promise<T>;
-export function promisify<A,A2,T>(f: (arg: A, arg2: A2, cb: (err: NodeJS.ErrnoException, res: T) => void) => void): (arg: A, arg2: A2) => Promise<T>;
-export function promisify<A,A2,A3,T>(f: (arg: A, arg2: A2, arg3: A3, cb: (err: NodeJS.ErrnoException, res: T) => void) => void): (arg: A, arg2: A2, arg3: A3) => Promise<T>;
-export function promisify<A,A2,A3,A4,T>(f: (arg: A, arg2: A2, arg3: A3, arg4: A4, cb: (err: NodeJS.ErrnoException, res: T) => void) => void): (arg: A, arg2: A2, arg3: A3, arg4: A4) => Promise<T>;
+export function promisify<T>(f: (cb: (err: any, res: T) => void) => void): () => Promise<T>;
+export function promisify<A,T>(f: (arg: A, cb: (err: any, res: T) => void) => void): (arg: A) => Promise<T>;
+export function promisify<A,A2,T>(f: (arg: A, arg2: A2, cb: (err: any, res: T) => void) => void): (arg: A, arg2: A2) => Promise<T>;
+export function promisify<A,A2,A3,T>(f: (arg: A, arg2: A2, arg3: A3, cb: (err: any, res: T) => void) => void): (arg: A, arg2: A2, arg3: A3) => Promise<T>;
+export function promisify<A,A2,A3,A4,T>(f: (arg: A, arg2: A2, arg3: A3, arg4: A4, cb: (err: any, res: T) => void) => void): (arg: A, arg2: A2, arg3: A3, arg4: A4) => Promise<T>;
 
 export function promisify(f) {
     return function() {
