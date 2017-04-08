0b58c5926f0a8fd2df201274de8553398ccaf671
whitecolor <alex.oshchepkov@gmail.com>
2017-04-09 03:24:03 +0500

    make callback optional

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-export function promisify<T>(f: (cb: (err: any, res: T) => void) => void, thisContext?: any): () => Promise<T>;
-export function promisify<A, T>(f: (arg: A, cb: (err: any, res: T) => void) => void, thisContext?: any): (arg: A) => Promise<T>;
-export function promisify<A, A2, T>(f: (arg: A, arg2: A2, cb: (err: any, res: T) => void) => void, thisContext?: any): (arg: A, arg2: A2) => Promise<T>;
-export function promisify<A, A2, A3, T>(f: (arg: A, arg2: A2, arg3: A3, cb: (err: any, res: T) => void) => void, thisContext?: any): (arg: A, arg2: A2, arg3: A3) => Promise<T>;
-export function promisify<A, A2, A3, A4, T>(f: (arg: A, arg2: A2, arg3: A3, arg4: A4, cb: (err: any, res: T) => void) => void, thisContext?: any): (arg: A, arg2: A2, arg3: A3, arg4: A4) => Promise<T>;
-export function promisify<A, A2, A3, A4, A5, T>(f: (arg: A, arg2: A2, arg3: A3, arg4: A4, arg5: A5, cb: (err: any, res: T) => void) => void, thisContext?: any): (arg: A, arg2: A2, arg3: A3, arg4: A4, arg5: A5) => Promise<T>;
+export function promisify<T>(f: (cb?: (err: any, res: T) => void) => void, thisContext?: any): () => Promise<T>;
+export function promisify<A, T>(f: (arg: A, cb?: (err: any, res: T) => void) => void, thisContext?: any): (arg: A) => Promise<T>;
+export function promisify<A, A2, T>(f: (arg: A, arg2: A2, cb?: (err: any, res: T) => void) => void, thisContext?: any): (arg: A, arg2: A2) => Promise<T>;
+export function promisify<A, A2, A3, T>(f: (arg: A, arg2: A2, arg3: A3, cb?: (err: any, res: T) => void) => void, thisContext?: any): (arg: A, arg2: A2, arg3: A3) => Promise<T>;
+export function promisify<A, A2, A3, A4, T>(f: (arg: A, arg2: A2, arg3: A3, arg4: A4, cb?: (err: any, res: T) => void) => void, thisContext?: any): (arg: A, arg2: A2, arg3: A3, arg4: A4) => Promise<T>;
+export function promisify<A, A2, A3, A4, A5, T>(f: (arg: A, arg2: A2, arg3: A3, arg4: A4, arg5: A5, cb?: (err: any, res: T) => void) => void, thisContext?: any): (arg: A, arg2: A2, arg3: A3, arg4: A4, arg5: A5) => Promise<T>;
 
 export function promisify(f: any, thisContext?: any) {
     return function () {
@@ -33,12 +33,12 @@ export function _try<T>(f: (arg: any, arg2: any, arg3: any, arg4: any) => T, arg
 
 export function _try(f: any, thisContext?: any) {
     let args = Array.prototype.slice.call(arguments);
-    return new Promise((res, rej) => {
+    return new Promise((resolve, reject) => {
         try {
             args.shift();
-            res(f.apply(thisContext, args));
+            resolve(f.apply(thisContext, args));
         } catch (err) {
-            rej(err);
+            reject(err);
         }
     });
 }
